f3ac28feb9a11e95f4638e54a14d4371a428ad54
Danny Sauer <danny.sauer@herodevs.com>
2026-09-16 17:25:27 -0500

    test: allow selecting another exact consumer Orb version

--- tests/test_repo_consistency.py
+++ tests/test_repo_consistency.py
@@ -414,8 +414,12 @@ class RepositoryConsistencyTests(unittest.TestCase):
                     local_orbs[f"{relative_path}:{name}"] = reference
 
         self.assertEqual(
-            references,
-            {".circleci/test-deploy.yml:released": PLANNED_PRODUCTION_ORB_REFERENCE},
+            set(references),
+            {".circleci/test-deploy.yml:released"},
+        )
+        self.assertRegex(
+            references[".circleci/test-deploy.yml:released"],
+            r"^vexcalibur-dev/vexcalibur@[0-9]+\.[0-9]+\.[0-9]+$",
         )
         self.assertEqual(
             local_orbs,
